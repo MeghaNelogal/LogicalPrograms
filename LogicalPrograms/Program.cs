165754fd7edcfc89bb72cb50163cf8c46ba8002f
Your Name <you@example.com>
2022-04-16 16:19:07 +0530

    [Megha] Add: Fibonacci Numbers Are Calculated

---
+++ LogicalPrograms/Program.cs
@@ -0,0 +1,30 @@
+﻿using LogicalPrograms;
+using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("welcome to Logicl Programs");
+        bool end = true;
+        Console.WriteLine("1. FibonacciSeries\n2. End the program");
+        while (end)
+        {
+            Console.WriteLine("\nTake an option to execute");
+            int option = Convert.ToInt32(Console.ReadLine());
+            switch (option)
+            {
+                case 1:
+                    FibonacciSeries fibonacci = new FibonacciSeries();
+                    fibonacci.CheckFibonacci();
+                    break;
+                case 2:
+                    end = false;
+                    break;
+
+                default:
+                    Console.WriteLine("Enter proper option to Execute");
+                    break;
+            }
+        }
+    }
+}
