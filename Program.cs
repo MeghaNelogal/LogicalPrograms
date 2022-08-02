9b65173d9264c824e08a97fe65ff23bd0df51766
Your Name <you@example.com>
2022-08-02 17:21:05 +0530

    [Megha] Add: Welcome To Logical Problems

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace LogicalProblems
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("***Welome To Logical Programs***");
+        }
+    }
+}
